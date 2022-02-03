d3247b92e3447cd090b80413f0883486f478d860
s8olheyd <s8olheyd@stud.uni-saarland.de>
2022-02-03 14:40:18 +0100

    v1.0 of automatic testing-script introduced

--- bachelor/run-tests.sh
+++ bachelor/run-tests.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 main() {
-    missions=("modified_copter") #"modified_copter_long")
+    
+    #missions=("modified_copter") #"modified_copter_long")
 
-    for i in "${missions[@]}"
+    for filename in missions/* #"${missions[@]}"
     do
-        move_mission "$i.txt"
+        move_mission $filename
 
-        echo "starting $i"
+        echo "starting $filename"
 
         if [[ $build == true ]]; then
-            ../Tools/autotest/autotest.py build.Copter test.Copter.Autofly --map
+            ../Tools/autotest/autotest.py build.Copter test.Copter.Autofly --speedup $speedup --map
         else 
-            ../Tools/autotest/autotest.py test.Copter.Autofly --map
+            ../Tools/autotest/autotest.py test.Copter.Autofly --speedup $speedup --map
         fi
 
-        move_logs $i
+        move_logs $filename
     done
+
+    cleanup
 }
 
 move_mission() {
@@ -24,26 +27,53 @@ move_mission() {
 }
 
 move_logs() {
+    filename=$(basename $1)
     lastlog="$(sed 's/\r$//' logs/LASTLOG.TXT)"
-    mv logs/0000000$lastlog.BIN $logfolder/$1".BIN"
+
+    mv logs/0000000$lastlog.BIN $logfolder/$filename".BIN"
 }
 
 cleanup() {
-    echo "cleanup"
+    rm -rf logs/
+    rm -rf terrain/
+    rm -rf test.ArduCopter/
+    rm *.tlog
+    rm eeprom.bin
 }
 
-while getopts l:s:b option
+inject_sm_attack() {
+    echo "TODO"
+}
+
+while getopts a:l:s:b option
 do
     case "${option}" in
         s) speedup=${OPTARG};;
         b) build=true;;
         l) logfolder=${OPTARG};;
+        a) attack=${OPTARG};;
     esac
 done
 
 if [[ -z $logfolder ]]; then
     echo "Logfolder must be specified!"
     exit 1
+fi
+
+if [[ -z $speedup ]]; then
+    echo "No Speedup specified, using 5"
+    speedup=5
+fi
+
+if [[ -z $attack ]]; then
+    echo "No attack specified, using Non-Attack-Version"
+    attack="none"
+elif [[ $attack == "switch-mode" ]]; then
+    echo "Building Switch-Mode attack"
+    attack="sm"
+else
+    echo "Unkown attack, using Non-Attack-Version"
+    attack="none"
 fi
 
 main
